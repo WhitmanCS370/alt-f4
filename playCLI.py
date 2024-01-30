9966a87dfa0883ee6dd8aafac19c92f5073b334f
Jack Allard <jackallard17@gmail.com>
2024-01-30 15:20:46 -0800

    implement audio file player CLI

---
+++ playCLI.py
@@ -0,0 +1,16 @@
+import sys
+import simpleaudio
+
+def play_sound(filename):
+    wave_obj = simpleaudio.WaveObject.from_wave_file(filename)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+if sys.argv[1] == '-p' or sys.argv[1] == '--play':
+    if sys.argv[2]:
+        print(f'Playing {sys.argv[2]}')
+        file = sys.argv[2]
+        play_sound(file)
+        
+    else:
+        print('no file provided...')
