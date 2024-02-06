b3e143b01eaf45ec5d56fa39246d88a261d16089
Coden Stark <codenstark@gmail.com>
2024-02-06 15:05:03 -0800

    updating help menu

--- playCLI.py
+++ playCLI.py
@@ -1,6 +1,8 @@
 import sys
 import simpleaudio
 
+argvlen = len(sys.argv)
+
 def play_sound(filename):
     wave_obj = simpleaudio.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
@@ -14,3 +16,13 @@ if sys.argv[1] == '-p' or sys.argv[1] == '--play':
         
     else:
         print('no file provided...')
+
+
+if argvlen<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
+
+    # This prints out a sample of how you might use this command
+    print("usage:",sys.argv[0], '[command] [arg(s)]')
+    print('--help, -h: lists help menu, what each function is and how to use it.')
+    print('--play, -p: plays sound(s) that are passed as argument(s).')
+
+    sys.exit(0);
