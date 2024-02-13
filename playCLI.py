89953bbe9c2e6e74f1cd70839454eafbda28adbf
Coden Stark <codenstark@gmail.com>
2024-02-13 15:28:06 -0800

    reordered to make help work again, added -ls

--- playCLI.py
+++ playCLI.py
@@ -16,6 +16,21 @@ def multi_sound(filenames):
     play_obj.wait_done()
 
 sounds = []
+
+if argvlen<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
+
+    # This prints out a sample of how you might use this command
+    print("usage:",sys.argv[0], '[command] [arg(s)]')
+    print('--help, -h        : lists help menu, what each function is and how to use it.')
+    print('--play, -p        : plays sound(s) that are passed as argument(s) in form \'directory/filename.wav\'.')
+    print('                  : how to play sounds sequentially?')
+    print('                  : how to play sounds overlapping?')
+    print('--rename, -r      : changes name of file (specified by first argument) to name specified in second argument.')
+    print('--list_sounds, -ls: lists all of the sounds/files in the default sounds directory')
+    print('                  : to list from non-default directories, the directory name should be passed as argument.')
+
+    sys.exit(0);
+
 if sys.argv[1] == '-p' or sys.argv[1] == '--play':
     if sys.argv[2]:
         for arg in sys.argv[2:]:
@@ -49,18 +64,6 @@ if sys.argv[1] == '-r' or sys.argv[1] == '--rename':
     else:
         print('no file provided...')
 
-
-if argvlen<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
-
-    # This prints out a sample of how you might use this command
-    print("usage:",sys.argv[0], '[command] [arg(s)]')
-    print('--help, -h  : lists help menu, what each function is and how to use it.')
-    print('--play, -p  : plays sound(s) that are passed as argument(s) in form \'directory/filename.wav\'.')
-    print('            : how to play sounds sequentially?')
-    print('            : how to play sounds overlapping?')
-    print('--rename, -r: changes name of file (specified by first argument) to name specified in second argument.')
-
-    sys.exit(0);
 
 def find_folder(folder):
     return(os.getcwd()+"/"+folder)
