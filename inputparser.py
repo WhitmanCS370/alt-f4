d2c1eeed12b9327e1a36369c3f97337efea7f3bf
Coden Stark <codenstark@gmail.com>
2024-03-31 15:15:56 -0700

    skeleton transform and parser parses

--- inputparser.py
+++ inputparser.py
@@ -2,27 +2,32 @@ import sys
 
 def parse(arglist):
     """Make a list of tuples for each command"""
-    # userin = []
+    transforms = []
+    plays = []
+    output = []
     flags = []
     other = []
     for item in arglist:
-        if "-" in item:
+        print(item)
+        if "--" in item:
+            print(1)
             flags.append(item)
+        if "--transform" in item:
+            print(2)
+            transforms.append(item)
+        if "--play" in item:
+            print(3)
+            plays.append(item)
+        if "--output" in item:
+            print(4)
+            output.append(item)
         else:
+            print(5)
             other.append(item)
-    
-    while i < len(arglist):
-        currentitem = arglist[i]
-        if "-" in currentitem:
-            flags.append((currentitem,i))   # add item and where it appears
-        else:
-            other.append((currentitem,i))   # add item and where it appears
-        print(arglist[i])
-        i = i + 1
     
     print("flags: ", end=''),
     
-    for x in range(len(flags)):
-        print(str(x) + " " + flags[x][0] + ", " + flags[x][1] + " ", end='')
+    for x in range(len(transforms)):
+        print(str(x) + " " + str(transforms[x]) + ", ", end='')
     print(" ")
 
