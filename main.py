4e5a5fbd12464f378c61003d22698478dd537eb5
Coden Stark <codenstark@gmail.com>
2024-04-10 14:17:40 -0700

    added comments to main and validation

--- main.py
+++ main.py
@@ -30,6 +30,14 @@ class main(cmd.Cmd):
         # init sound editing file.
 
     def validate(self, inputType, args):
+        """Validate commands passed by the user.
+        Using the 'validation' module, it checks if the type of command
+        exists and if the passed arguments work with the command.
+
+        Arguments:
+        inputType-- the type of command the user is trying to run.
+        args-- all other parts of the user's input.
+        """
         if inputType in self.commandDict.keys():
             validator = self.commandDict[inputType](args)
             return validator
@@ -38,6 +46,18 @@ class main(cmd.Cmd):
             return False
 
     def parse_play(self, input):
+        """Parse through play commands.
+        There are different ways to play sounds, and input can be
+        complicated. 
+
+        Arguments:
+        input-- the arguments the user passed in their command call.
+
+        Returns:
+        flags-- a list of flags that were paseed (what type of play to do)
+        sounds-- a list of sounds to play
+        delay-- how much delay between sounds (delay play exclusive)
+        """
         flags = []
         sounds = []
         delay = None
@@ -56,6 +76,7 @@ class main(cmd.Cmd):
 
     def do_play(self, args):
         """Play sound(s), either all at once or sequentially (with or without delay).
+        Implementation handled in AudioPlayer.
         usage) play [multi|seq|delay={delaytime}] <file_name(s)>
         """
         # TODO: add error catching for if --delay=(something other than float)
@@ -68,6 +89,7 @@ class main(cmd.Cmd):
 
     def do_rename(self, args):
         """Rename an audio file.
+        Implementation handled in FileManager.
         usage) rename <original_file> <new_name>        
         """
         if(self.validate("rename", args)):
@@ -77,6 +99,7 @@ class main(cmd.Cmd):
 
     def do_add_sound(self, args):
         """Add sound to audio archive.
+        Implementation handled in FileManager.
         usage) add_sound <folder_to_add_to> <path_to_original_file>
         """
         if(self.validate("add_sound", args)):
@@ -86,6 +109,7 @@ class main(cmd.Cmd):
 
     def do_remove_sound(self, args):
         """Remove sound from audio archive.
+        Implementation handled in FileManager.
         usage) remove_sound <path_to_file>
         """
         # TODO: implement validate_remove_sound in validation.py and make sure it works as intended
@@ -96,6 +120,7 @@ class main(cmd.Cmd):
 
     def do_list_sounds(self, args = "sounds"):
         """List sounds in specified folder.
+        Implementation handled in FileManager.
         usage) list_sounds <folder>
         """
 
--- validation.py
+++ validation.py
@@ -1,5 +1,10 @@
+# argument checking is done at lower level because returning false here sends control
+# back to the function that called it and will throw an error message that's more 
+# useful to the user.
+
 def validate_play(args = None):
     """Validates the play command.
+    Checks to see that the user passed arguments.
     """
     # TODO: check if first thing is a sound or flag (maybe)
     if not args:
@@ -8,6 +13,7 @@ def validate_play(args = None):
     
 def validate_rename(args):
     """Validates the rename command.
+    Checks to see that the user passed arguments.
     """
     # TODO: think if there's other things we need to validate
     input = args.split(" ")
@@ -17,15 +23,16 @@ def validate_rename(args):
     
 def validate_list_sounds(args):
     """Validates the list_sounds command.
+    Checks to see that the user passed arguments.
     """
     # TODO: think if there's other things we need to validate
-    input = args.split(" ")
-    if len(input)==1 and input[0]=="":
+    if not args:
         return False
     return True
 
 def validate_add_sound(args):
     """Validates the add_sound command.
+    Checks to see that the user passed arguments.
     """
     # TODO: implement, move validation stuff in the function to here.
     if not args:
@@ -34,6 +41,7 @@ def validate_add_sound(args):
 
 def validate_remove_sound(args=None):
     """Validates the remove_sound command.
+    Checks to see that the user passed arguments.
     """
     # TODO: implement
     if not args:
@@ -42,6 +50,7 @@ def validate_remove_sound(args=None):
 
 def validate_delay(args):
     """Validates the delay subcommand of play command.
+    Checks to see that the user passed arguments.
     """
     # TODO: implement
     if not args:
