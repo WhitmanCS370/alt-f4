dbeec382121bea832df174d5b4e4acac79b41252
samuelboerner <boernersam@gmail.com>
2024-04-29 22:33:25 -0700

    add buttons

--- main_gui.py
+++ main_gui.py
@@ -12,6 +12,7 @@ class AudioArchiveApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Audio Archive Manager')
+        self.geometry('600x400')
 
         # Initialize components
         self.player = AudioPlayer(self)
@@ -22,7 +23,25 @@ class AudioArchiveApp(tk.Tk):
         self.create_widgets()
 
     def create_widgets(self):
-        pass
+        # Buttons
+        play_button = tk.Button(self, text='Play Sound', command=self.play_sound)
+        play_button.pack(pady=10)
+
+        add_sound_button = tk.Button(self, text='Add Sound', command=self.add_sound)
+        add_sound_button.pack(pady=10)
+
+        rename_button = tk.Button(self, text='Rename Sound', command=self.rename_sound)
+        rename_button.pack(pady=10)
+
+        merge_button = tk.Button(self, text='Merge Sounds', command=self.merge_sounds)
+        merge_button.pack(pady=10)
+
+        list_sounds_button = tk.Button(self, text='List Sounds', command=self.list_sounds)
+        list_sounds_button.pack(pady=10)
+
+        # Quit button
+        quit_button = tk.Button(self, text='Quit', command=self.quit)
+        quit_button.pack(pady=10)
 
     def play_sound(self):
         pass
